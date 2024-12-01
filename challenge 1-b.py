f2603776a406961d72753669cc82f4fb3bfa302e
Charbel Estephan <charbelstephan12@gmail.com>
2024-12-01 15:30:22 +0200

    Day 1

---
+++ challenge 1-b.py
@@ -0,0 +1,24 @@
+# Open and read the file
+file_path = "New Text Document.txt"  # Replace with the actual path if needed
+with open(file_path, "r") as file:
+    data = file.readlines()
+
+# Split into two lists
+list1, list2 = [], []
+for line in data:
+    if line.strip():  # Ensure the line is not empty
+        col1, col2 = map(int, line.split())  # Convert both columns to integers
+        list1.append(col1)
+        list2.append(col2)
+
+list1.sort()
+list2.sort()
+
+sum = 0
+for x in list1:
+    occurences = list2.count(x)
+    add = x * occurences
+    sum += add
+
+
+print(sum)
